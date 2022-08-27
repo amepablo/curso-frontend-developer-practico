ac82f955d039538a385400dc7e7e7e8b019ac43d
Pablo <amepablo@gmail.com>
2022-08-27 14:14:01 -0300

    Carrito de compras

--- main.js
+++ main.js
@@ -1,16 +1,37 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toogleDesktopMenu);
 menuHamIcon.addEventListener('click', toogleMenuMenu);
+menuCarritoIcon.addEventListener('click', toogleCarritoAside);
+
 
 function toogleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
 };
 
 function toogleMenuMenu () {
-    mobileMenu.classList.toggle('inactive');
 
+    const isAsideClosed = aside.classList.contains('inactive');
+
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
+
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toogleCarritoAside () {
+    const ismobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+    if (!ismobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+    }
+
+     aside.classList.toggle('inactive');
 }
